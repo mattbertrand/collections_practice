294bcb402b6257f07a16b3f839f08e002b4687dd
github username <github email address>
2020-11-14 14:27:04 -0800

    commit tree 47c0a210c010358236627c8d7746dd6e372497fa

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,54 @@
+def sort_array_asc(numbers)
+    numbers.sort do |a, b|
+        a <=> b
+    end
+end
 
+def sort_array_desc(numbers)
+    numbers.sort do |a, b|
+        b <=> a
+    end
+end
+
+def sort_array_char_count(words)
+    words.sort do |first, second|
+        first.length <=> second.length
+    end
+end
+
+def swap_elements(array)
+    array[1], array[2] = array[2], array[1]
+    array
+end
+
+def reverse_array(array)
+    array.reverse do |a, b|
+        a > b
+    end
+end
+
+def kesha_maker(words)
+    words.each do |word|
+        word[2]= "$"
+    end
+end
+
+def find_a(letters)
+    letters.select do |word|
+        word[0] == "a"
+    end
+end
+
+def sum_array(array)
+    array.inject{|sum, number| sum + number}
+end
+
+def add_s(array)
+    array.collect do |word|
+        if array[1] == word
+            word
+        else
+            word + "s"
+        end
+    end
+end
